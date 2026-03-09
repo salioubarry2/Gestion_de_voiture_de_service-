d510ca875e2ee186c0fadcb9bb938413928edba7
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 21:20:49 -0400

    la classe voiture et ses attributs sont ajouté

---
+++ voiture.py
@@ -0,0 +1,7 @@
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage,):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
