67e1efdf8dc812ca85b8356ddea9dc1371d0e948
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 21:11:16 -0400

    la methode affecter_voiture  de la classe employer ajouté

--- employe.py
+++ employe.py
@@ -14,3 +14,14 @@ class Employe:
             print("Voiture:", self.voitureService.marque, self.voitureService.nom, self.voitureService.matricule)
         else:
             print("Aucune voiture assignée")
+
+    def affecter_voiture(self, voiture):
+        if self.voitureService is not None:
+            print("cet employer a deja une voiture ")
+        elif voiture.chauffeur is not None:
+            print("cette voiture est deja assignée ")
+        else :
+            self.voitureService = voiture
+            voiture.chauffeur = self
+            print("voiture est deja assignée")
+
