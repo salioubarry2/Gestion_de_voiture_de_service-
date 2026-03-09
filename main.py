470078f57c1666111240788866584777e9acdc6b
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 21:38:05 -0400

    Creation des instances Employer et Voiture dans main

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from employe import Employe
 from voiture import Voiture
 
+emp1=Employe("1","Barry","Saliou")
+emp2=Employe("2","Diallo","Ali")
+
+v1=Voiture("A1",2026,"NISAN",5500)
+v2=Voiture("A2",2026,"TOYOTA",7000)
+
