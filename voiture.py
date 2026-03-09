2ccb62871b139852ebbaf17ba419cb2ed0cce961
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 21:26:26 -0400

    la methode afficher_information de la  classe voiture est ajouté

--- voiture.py
+++ voiture.py
@@ -5,3 +5,14 @@ class Voiture:
         self.marque = marque
         self.kilometrage = kilometrage
         self.chauffeur = None
+
+    def afficher_information(self):
+        print("voiture:",self.marque)
+        print("annee:",self.annee)
+        print("matricul:",self.matricule)
+        print("kilometrage:",self.kilometrage)
+
+        if self.chauffeur:
+            print("chauffeur:",self.chauffeur.nom)
+        else:
+            print("Aucun chauffeur")
