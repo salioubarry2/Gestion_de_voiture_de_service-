2a17ac718ad9415db9d305db06eb8f50fab65a5b
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 21:04:08 -0400

    la methode afficher_information de la classe employer ajouté

--- employe.py
+++ employe.py
@@ -5,3 +5,12 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voitureService = None
+
+
+    def afficher_information(self):
+        print("Employer:", self.nom , self.prrenom)
+        print("Permis:", self.numeroPermis)
+        if self.voitureService:
+            print("Voiture:", self.voitureService.marque, self.voitureService.nom, self.voitureService.matricule)
+        else:
+            print("Aucune voiture assignée")
