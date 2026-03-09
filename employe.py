6250d0ed587633f701c1dc60f0922982eb528de6
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-03-08 20:56:28 -0400

    la classe Employer et ses attrubits sont ajouté

---
+++ employe.py
@@ -0,0 +1,7 @@
+class Employe:
+
+    def __init__(self, numeroPermis, nom, prenom):
+        self.numeroPermis = numeroPermis
+        self.nom = nom
+        self.prenom = prenom
+        self.voitureService = None
